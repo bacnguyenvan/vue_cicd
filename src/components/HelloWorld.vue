973dc9035c01e083041551d8734a1b107caaf65c
bacDev <bacb4tvk@gmail.com>
2023-05-07 13:30:07 +0700

    fix ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="body">
       <div class="item">
-        <p class="music-name">GRATEFUL - NEFFEX - HTK</p>
+        <p class="music-name">GRATEFUL - NEFFEX</p>
       </div>
       <div class="music-block">
         <div class="prev">
